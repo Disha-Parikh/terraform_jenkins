83066b5e78aa3095da45093524167e517262b87e
Disha-Parikh <dishaparikh98@gmail.com>
2020-04-10 17:15:25 +0530

    using tls to generate private key

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,6 +1,11 @@
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits = 4096
+}
+
 resource "aws_key_pair" "key_pair" {
   key_name = var.key_name
-  public_key = file(var.public_key_path)
+  public_key = "${tls_private_key.example.public_key_openssh}"
 
 }
 
@@ -10,14 +15,14 @@ resource "aws_instance" "public_instance" {
   availability_zone = var.public_az
   subnet_id = var.public_subnet
   security_groups = [var.sg]
-  key_name = aws_key_pair.key_pair.key_name
+  key_name = "${aws_key_pair.key_pair.key_name}"
   provisioner "file" {
     source = "install.sh"
     destination = "/tmp/install.sh"
     connection {
       user = "ec2-user"
       host = aws_instance.public_instance.public_ip
-      private_key = file("./aws_key.pub")
+      private_key = tls_private_key.example.private_key_pem
     }
   }
   provisioner "remote-exec" {
