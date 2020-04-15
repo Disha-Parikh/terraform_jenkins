7079902d89ca7e32c248feddf7d2b0bbf27fa82a
Disha-Parikh <dishaparikh98@gmail.com>
2020-04-15 18:48:18 +0530

    SDSFDSF

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo service docker start
 sudo usermod -a -G docker ec2-user
 echo "VERSION"
 sudo docker --version
-sudo docker login -p ${docker}
-sudo docker pull dishaparikh98/finalflask
+sudo docker login -u dishaparikh98 -p ${docker}
+sudo docker pull dishaparikh98/finalflask:2c25611
 sudo docker images
-sudo docker run dishaparikh98/finalflask -d
+sudo docker run dishaparikh98/finalflask:2c25611 -d
