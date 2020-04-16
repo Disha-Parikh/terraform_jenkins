31592c6d790a31611b7accb43c0fa15a626c4f1b
Disha-Parikh <dishaparikh98@gmail.com>
2020-04-16 17:48:08 +0530

    ADSFF

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 sudo yum update -y
 sudo amazon-linux-extras install docker -y
+sudo apt-get install -y postgresql
+echo "CREATE ROLE postgres LOGIN ENCRYPTED PASSWORD 'einfochips';" | sudo -u postgres psql
+su postgres -c "createdb db1 --owner postgres"
+service postgresql reload
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 echo "VERSION"
