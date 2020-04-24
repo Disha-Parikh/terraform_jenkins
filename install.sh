9d0771902d6732c3220cecf14bc4723bcc945526
Disha-Parikh <dishaparikh98@gmail.com>
2020-04-24 11:45:26 +0530

    SSDFASF

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ sudo yum install -y postgresql-server postgresql-devel
 sudo service postgresql initdb
 echo "OHH"
 sudo sed -i "s/[#]listen_addresses=[']/listen_addresses=\'localhost/1" /var/lib/pgsql/data/postgresql.conf
+sudo cat /var/lib/pgsql/data/postgresql.conf
 sudo systemctl enable postgresql
 sudo systemctl start postgresql
 sudo service docker start
