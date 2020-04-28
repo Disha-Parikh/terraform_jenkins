68c0d011014dbc3f17a1d2e3300bd5e69071d21b
Disha-Parikh <dishaparikh98@gmail.com>
2020-04-28 11:24:50 +0530

    opened 5002

--- vpc/main.tf
+++ vpc/main.tf
@@ -23,9 +23,9 @@ resource "aws_security_group" "custom_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port = 5000
+    from_port = 5002
     protocol = "tcp"
-    to_port = 5000
+    to_port = 5002
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
